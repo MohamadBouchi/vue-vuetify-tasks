db6e66b43969f95bf2777e7d4c1fdaacb32a788b
Mohamed Bouchi <mohamedbouchi@Mohameds-iMac.local>
2019-02-03 11:56:57 +0100

    team page

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,14 +4,20 @@
     <v-container class="my-5">
 
       <v-layout row class="mb-3">
-        <v-btn @click="sortBy('title')" small flat color="grey">
-          <v-icon left small>folder</v-icon>
-          <span class="caption text-lowercase">by project name</span>
-        </v-btn>
-        <v-btn small flat color="grey" @click="sortBy('person')">
-          <v-icon left small>person</v-icon>
-          <span class="caption text-lowercase">by person</span>
-        </v-btn>
+        <v-tooltip top>
+          <v-btn slot="activator" @click="sortBy('title')" small flat color="grey">
+            <v-icon left small>folder</v-icon>
+            <span class="caption text-lowercase">by project name</span>
+          </v-btn>
+          <span>sort project by project name</span>
+        </v-tooltip>
+        <v-tooltip top>
+          <v-btn small slot="activator" flat color="grey" @click="sortBy('person')">
+            <v-icon left small>person</v-icon>
+            <span class="caption text-lowercase">by person</span>
+          </v-btn>
+          <span>sort by person</span>
+        </v-tooltip>
       </v-layout>
       <v-card flat v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,7 +2,27 @@
   <div class="team">
     <h1 class="subheading grey--text">team page</h1>
     <v-container class="my-5">
-        content
+        <v-layout row wrap>
+          <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+            <v-card flat class="text-xs-center ma-3">
+              <v-responsive class="pt-4">
+                <v-avatar size="100" class="grey lighten-2">
+                  <img :src="person.avatar">
+                </v-avatar>
+              </v-responsive>
+              <v-card-text>
+                <div class="subheading">{{ person.name }}</div>
+                <div class="grey--text">{{ person.role }}</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn flat color="grey">
+                  <v-icon small left>message</v-icon>
+                  <span>message</span>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
     </v-container>
   </div>
 </template>
@@ -11,6 +31,16 @@
 
   export default {
     components: {
+    },
+    data() {
+      return {
+        team: [
+          {name: 'mohamed', role: 'admin', avatar:'/avatar-1.png'},
+          {name: 'timo', role: 'developer', avatar:'/avatar-2.png'},
+          {name: 'arndt', role: 'admin', avatar:'/avatar-3.png'},
+          {name: 'ralf', role: 'backend devoloper', avatar:'/avatar-4.png'},
+          {name: 'lukas', role: 'backend developer', avatar:'/avatar-5.png'}        ]
+      }
     }
   }
 </script>
