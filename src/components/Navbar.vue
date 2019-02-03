2e2424cb0c8ce8adedfc2a771d530c60b65293b4
Mohamed Bouchi <mohamedbouchi@Mohameds-iMac.local>
2019-02-03 19:34:37 +0100

    date fns package installed add new project popup with validation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,18 @@
                 <span>App</span>
             </v-toolbar-title>
             <v-spacer></v-spacer>
+
+            <v-menu offset-y>
+                <v-btn flat slot="activator" color="grey">
+                    <v-icon left>expand_more</v-icon>
+                    <span>menu</span>
+                </v-btn>
+                <v-list>
+                    <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+                        <v-list-tile-title>{{ link.text }}</v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-menu>
             <v-btn flat color="grey">
                 <span>Sign out</span>
                 <v-icon right>exit_to_app</v-icon>
@@ -19,6 +31,9 @@
                         <img src="/avatar-1.png">
                     </v-avatar>
                     <p class="white--text subheading mt-1">mohamed su</p>
+                </v-flex>
+                <v-flex class="mt-4 mb-3">
+                    <Popup />
                 </v-flex>
             </v-layout>
             <v-list>
@@ -36,7 +51,10 @@
 </template>
 
 <script>
+import Popup from './Popup'
+
 export default {
+    components: { Popup },
     data() {
         return {
             drawer: false,
---
+++ src/components/Popup.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-dialog max-width="600px">
+        <v-btn flat slot="activator" class="success">add new project</v-btn>
+        <v-card>
+            <v-card-title>
+                <h2>add a new project</h2>
+            </v-card-title>
+            <v-card-text>
+                <v-form class="px-3" ref="form">
+                    <v-text-field label="title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+                    <v-textarea label="info" v-model="content" prepend-icon="edit" :rules="inputRules"></v-textarea>
+                    <v-menu>
+                        <v-text-field :value="formatedDate" slot="activator" label="date" prepend-icon="date_range"></v-text-field>
+                        <v-date-picker v-model="due"></v-date-picker>
+                    </v-menu>
+                    <v-btn flat class="success right mx-0 mt-4" @click="submit">add project</v-btn>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+import format from 'date-fns/format';
+
+export default {
+    data() {
+        return {
+            title: '',
+            content: '',
+            due: null,
+            inputRules: [
+                V => V.length >= 3 || 'Minimum is 3'
+            ]
+        }
+    },
+    methods: {
+        submit() {
+            if(this.$refs.form.validate())
+                console.log(this.title, this.content);
+        }
+    },
+    computed: {
+        formatedDate() {
+            return this.due ? format(this.due, 'Do MMM YYYY') : ''
+        }
+    }
+}
+</script>
