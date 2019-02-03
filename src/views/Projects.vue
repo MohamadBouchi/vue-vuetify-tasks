b88051a80e33db096f414685d2ad77668c6a7887
Mohamed Bouchi <mohamedbouchi@Mohameds-iMac.local>
2019-02-03 12:07:48 +0100

    projects page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,7 +2,19 @@
   <div class="projects">
     <h1 class="subheading grey--text">projects page</h1>
     <v-container class="my-5">
-          content
+          <v-expansion-panel>
+            <v-expansion-panel-content v-for="project in myProjects" :key="project.title">
+              <div slot="header">
+                {{ project.title }}
+              </div>
+              <v-card>
+                <v-card-text class="px-4 grey--text">
+                  <div class="font-weight-bold">due by {{ project.due }}</div>
+                  <div>{{ project.content }}</div>
+                </v-card-text>
+              </v-card>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
     </v-container>
   </div>
 </template>
@@ -11,6 +23,24 @@
 
   export default {
     components: {
+    },
+    data() {
+      return {
+          projects: [
+            { title: 'Design a new website', person: 'mohamed', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Design a new second website', person: 'mohamed', due: '11st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          ]
+      }
+    },
+    computed: {
+      myProjects() {
+        return this.projects.filter(project => {
+          return project.person === 'mohamed'
+        });
+      }
     }
   }
 </script>
